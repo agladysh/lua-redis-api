7a0786c309c6bad3f6c7a4f4320755293ce737cb
Alexander Gladysh <agladysh@gmail.com>
2011-03-29 07:58:17 +0400

    etc/generate: rewritten
    Still, not banana.
    
    Looks like that Redis commands are just not generator-friendly.

--- etc/generate.lua
+++ etc/generate.lua
@@ -41,10 +41,22 @@ local is_table,
         'is_string'
       }
 
-local empty_table
+local assert_is_table,
+      assert_is_string,
+      assert_is_boolean
+      = import 'lua-nucleo/typeassert.lua'
+      {
+        'assert_is_table',
+        'assert_is_string',
+        'assert_is_boolean'
+      }
+
+local empty_table,
+      tclone
       = import 'lua-nucleo/table-utils.lua'
       {
-        'empty_table'
+        'empty_table',
+        'tclone'
       }
 
 local make_concatter,
@@ -97,14 +109,101 @@ local ident_map = setmetatable(
 
 local COMMANDS = { }
 do
+  -- Load and preprocess data
+
   local data_str = assert(io.open("commands.json", "r")):read("*a")
   local data = assert(json.decode(data_str))
   for name, info in pairs(data) do
-    assert(info.name == nil)
-    info.name = name:upper()
-    info.id = ident_map[name:lower():gsub(" ", "_")]
-    info.arguments = info.arguments or empty_table
-    COMMANDS[#COMMANDS + 1] = info
+    local cmd = { }
+
+    cmd.raw = info -- TODO: Remove
+
+    cmd.name = name:upper()
+    cmd.id = ident_map[name:lower():gsub(" ", "_")]
+
+    cmd.summary = assert_is_string(info.summary)
+    cmd.since = assert_is_string(info.since)
+    cmd.group = assert_is_string(info.group)
+
+    cmd.have_multiple = false
+    cmd.post_multiple_count = 0
+    cmd.multiple_is_optional = false
+
+    cmd.arguments = { }
+    if info.arguments then
+      for i = 1, #info.arguments do
+        local arg_info = info.arguments[i]
+
+        local arg = { }
+
+        if arg_info.optional ~= nil then
+          arg.optional = assert_is_boolean(arg_info.optional)
+          if arg.optional and cmd.have_multiple then
+            error(
+                "optional after multiple in " .. name .. ":" .. tstr(cmd)
+              )
+          end
+        else
+          arg.optional = false
+        end
+
+        if arg_info.multiple then
+          arg.multiple = true
+
+          if cmd.have_multiple then
+            error(
+                "double multiple in " .. name .. ":" .. tstr(cmd)
+              )
+          end
+
+          if arg.optional then
+            cmd.multiple_is_optional = false
+          end
+
+          cmd.have_multiple = true
+        else
+          arg.multiple = false
+
+          if cmd.have_multiple then
+            if cmd.multiple_is_optional then
+              error(
+                  "optional post-multiple in " .. name .. ":" .. tstr(cmd)
+                )
+            end
+
+            cmd.post_multiple_count = cmd.post_multiple_count + 1
+          end
+        end
+
+        if not is_table(arg_info.name) then
+          arg.name = assert_is_string(arg_info.name)
+          arg.id = ident_map[assert_is_string(arg_info.name)]
+          arg.type = assert_is_string(arg_info.type) -- TODO: map types
+
+          cmd.arguments[#cmd.arguments + 1] = arg
+        else
+          -- TODO: Do not lose group info
+          --       (for example, about paired optionals like in ZRANGEBYSCORE)
+
+          assert_is_table(arg_info.type)
+          assert(#arg_info.name > 0)
+          assert(#arg_info.name == #arg_info.type)
+          assert(arg_info.optional or arg_info.multiple)
+
+          for i = 1, #arg_info.name do
+            arg.name = assert_is_string(arg_info.name[i])
+            arg.id = ident_map[assert_is_string(arg_info.name[i])]
+            arg.type = assert_is_string(arg_info.type[i]) -- TODO: map types
+
+            cmd.arguments[#cmd.arguments + 1] = arg
+
+            arg = tclone(arg)
+          end
+        end
+      end
+    end
+
+    COMMANDS[#COMMANDS + 1] = cmd
   end
 
   table.sort(COMMANDS, function(lhs, rhs) return lhs.name < rhs.name end)
@@ -116,95 +215,104 @@ local cat, concat = make_concatter()
 
 --------------------------------------------------------------------------------
 
-local ocat = cat
-local function cat(s)
-  if type(s) ~= "string" then print(tstr(s)) end
-  assert(type(s) == "string")
-  ocat(s)
-  return cat
+for i = 1, #COMMANDS do
+  local cmd = COMMANDS[i]
+
+  cat [[
+--------------------------------------------------------------------------------
+-- ]] (cmd.name) [[
+
+--
+-- Group: ]] (cmd.group) [[
+
+-- Since: ]] (cmd.since) [[
+
+--
+]] (wrap(cmd.summary, 80 - 3, "-- ", "-- ")) [[
+
+--------------------------------------------------------------------------------
+
+]]
+  if #cmd.arguments == 0 then
+    cat [[
+local ]] (cmd.id) [[ = function(self)
+  return self.command_(self.obj_, ]] (("%q"):format(cmd.name)) [[)
 end
 
-for i = 1, #COMMANDS do
-  local info = COMMANDS[i]
-
-  cat [[
---------------------------------------------------------------------------------
--- ]] (info.name) [[
-
---
--- Group: ]] (info.group) [[
-
--- Since: ]] (info.since) [[
-
---
-]] (wrap(info.summary, 80 - 3, "-- ", "-- ")) [[
-
---------------------------------------------------------------------------------
-
-local ]] (info.id) [[ = function(
+]]
+  else
+    cat [[
+local ]] (cmd.id) [[ = function(
     self]]
-
-  local multiple = false
-  local genuine_multiple = false
-  for i = 1, #info.arguments do
-    local arg = info.arguments[i]
-
-    arg.auto_multiple = true -- TODO: Hack! Remove ASAP.
-
-    if is_table(arg.name) and not arg.multiple then
-      io.stderr:write(
-          "WARNING: hidden multiple cmd `", info.name,
-          "' arg ", tstr(arg), "\n"
-        )
-      arg.auto_multiple = true
-    end
-
-    if arg.multiple then
-      assert(genuine_multiple == false, "double multiple")
-      genuine_multiple = true
-    end
-
-    if arg.multiple or arg.auto_multiple then
-      if not multiple then
-        multiple = true
+    for i = 1, #cmd.arguments do
+      local arg = cmd.arguments[i]
+      if arg.multiple then
+        cat [[,
+    ...]]
+        break
+      end
 
         cat [[,
-    ...
-    -- ]] (ident_map[maybe_tstr(arg.name)])
-      else
-        io.stderr:write(
-            "WARNING: double multiple cmd `", info.name,
-            "' arg ", tstr(arg), "\n"
-          )
-        cat [[
+    ]] (arg.id)
 
-    -- ]] (ident_map[maybe_tstr(arg.name)])
-      end
-    else
-      if multiple then
-        cat [[
-
-    -- ]] (ident_map[arg.name])
-      else
-
-        cat [[,
-    ]] (ident_map[arg.name])
-      end
     end
-  end
 
-  cat [[
+    cat [[
 
   )
+  local nargs = select("#", ...)
+]]
+    -- TODO: Try to make generated code to look less weird
+    if command.have_multiple then
+      local had_multiple = false
+      for i = 1, #cmd.arguments do
+        if arg.multiple then
+          assert(not had_multiple)
+          had_multiple = true
+
+          cat [[
+  for i = ]] (i) [[, nargs - ]] (cmd.post_multiple_count) [[ do
+    check_]] (arg.type) [[(self, ]] (
+        tostring(arg.optional)
+      ) [[, select(nargs - ]] (i) [[, ...))
+  end
+]]
+        elseif had_multiple then
+          assert(not arg.optional)
+          cat [[
+  check_]] (arg.type) [[(self, false, select(nargs - ]] (i) [[, ...))
+]]
+        end
+      end
+    end
+
+    cat [[
   return self.command_(
-      self.obj_, ]] (("%q"):format(info.name)) [[,
+      self.obj_
+]]
+
+    for i = 1, #cmd.arguments do
+      local arg = cmd.arguments[i]
+      if arg.multiple then
+        cat [[,
       ...
 ]]
-  cat [[
+        break
+      end
+
+      -- TODO: This is wrong!
+      cat [[,
+      check_]] (arg.type) [[(self, ]] (
+          tostring(arg.optional)
+        ) [[, ]] (arg.id) [[)]]
+    end
+
+    cat [[
     )
 end
 
 ]]
+  end
 end
 
 io.write(concat(), "\n")
